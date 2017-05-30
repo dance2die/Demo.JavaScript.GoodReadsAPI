b40febc8ec7cf61bfde0bf1b78adb2e3dda34717
dance2die <dontbothermewithspam@gmail.com>
2017-05-30 16:12:46 -0400

    set up webdev server

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+// https://www.slightedgecoder.com/2017/05/22/setting-es6-environment-asp-net-mvc-5/
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './js/dist')
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    // https://github.com/StephenGrider/ReduxSimpleStarter/blob/master/webpack.config.js
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    }
+}
