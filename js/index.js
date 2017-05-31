e08810ad988c0bbb0ec306399d9e00504b1f51ff
dance2die <dontbothermewithspam@gmail.com>
2017-05-31 13:30:42 -0400

    refactored code

--- js/index.js
+++ js/index.js
@@ -5,66 +5,30 @@ var apiConfig = require('../apikey.js');;
 var log = `
     goodreadsKey=${apiConfig.goodreadsKey};
     goodreadsSecret=${apiConfig.goodreadsSecret}`;
-// alert(log);
 
 $(document).ready(() => {
     const url = `https://www.goodreads.com/shelf/list.xml`;
-    var data = `key=${apiConfig.goodreadsKey}&user_id=${apiConfig.goodreadsUserID}&page=1`;
-    // var data = {
-    //     key: apiConfig.goodreadsKey,
-    //     user_id: apiConfig.goodreadsUserID,
-    //     page: 1
-    // };
-    var queryUrl = `${url}?${data}`;
-    // var q = encodeURI("select * from xml where url=\"").replace(/%20/g, "+") + queryUrl + "\"";
-    var q = `select * from xml where url="${queryUrl}"`;
-
+    // https://stackoverflow.com/a/22281596/4035
     const yqlUrl = "http://query.yahooapis.com/v1/public/yql";
-    // var uri = `${yqlUrl}&q=${q}`;
-    // var uri = yqlUrl + "?q=" + encodeURIComponent(q).replace(/%20/g, "+")
+
+    let data = `key=${apiConfig.goodreadsKey}&user_id=${apiConfig.goodreadsUserID}&page=1`;
+    let queryUrl = `${url}?${data}`;
+    let q = `select * from xml where url="${queryUrl}"`;
 
     $.ajax({
         type: "get",
         url: yqlUrl,
         data: {
-            // q:"select * from xml where url=\""+queryUrl+"\"",
             q: q,
             format: "xml"
         },
-        // dataType: 'xml',
-        // crossDomain: true,
-        // contentType: "application/xml; charset=utf-8",
         success: (response) => {
-            debugger;
             alert(response);
         },
         error:(request, status, error) => {
-            debugger;
             alert(`error: ${error}`);
         }
     });
-
-// https://stackoverflow.com/a/22281596/4035
-// var url = `https://www.goodreads.com/shelf/list.xml?key=${apiConfig.goodreadsKey}&user_id=${apiConfig.goodreadsUserID}&page=1`;
-
-// $.get("http://query.yahooapis.com/v1/public/yql",
-//     {
-//         q: "select * from xml where url=\""+url+"\"",
-//         format: "xml"
-//     },
-//     function(xml){
-//         // contains XML with the following structure:
-//         // <query>
-//         //   <results>
-//         //     <GoodreadsResponse>
-//         //        ...
-
-//         console.log(xml);
-//     }
-// );
-
-
-
 });
 
 
