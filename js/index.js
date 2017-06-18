eab7e29ef1a8bb06165da16392a54429185d6a7c
Sung <sungmkim80@gmail.com>
2017-06-18 13:43:04 -0400

    created a YQL URL using proxify-url

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ var path = require('path');
 var $ = require('jquery');
 var apiConfig = require('../apikey.js');
 var defiant = require('defiant');
+var proxify = require('proxify-url');
 import YqlAjax from './yql_ajax';
 
 var log = `
@@ -9,20 +10,64 @@ var log = `
     goodreadsSecret=${apiConfig.goodreadsSecret}`;
 
 $(document).ready(() => {
-    const url = `https://www.goodreads.com/shelf/list.xml`;
+    const goodReadsURL = `https://www.goodreads.com/shelf/list.xml`;
     // https://stackoverflow.com/a/22281596/4035
-    let data = `key=${apiConfig.goodreadsKey}&user_id=${apiConfig.goodreadsUserID}&page=1`;
-    let goodReadsURL = `${url}?${data}`;
+    let q = `key=${apiConfig.goodreadsKey}&user_id=${apiConfig.goodreadsUserID}&page=1`;
+    let url = `${goodReadsURL}?${q}`;
 
-    let yqlAjax = new YqlAjax();
-    yqlAjax.ajax(goodReadsURL).done((yqlResponse) => {
-        console.log("success 111");
+    // let yqlAjax = new YqlAjax();
+    // yqlAjax.ajax(goodReadsURL).done((yqlResponse) => {
+    //     console.log("success 111");
+    //     // DefiantJS XPath query for user shelf for "read" section.
+    //     let search = JSON.search(yqlResponse, "//*/user_shelf[name='read']");
+    //     console.log(search);
+    // }).fail((request, status, error) => {
+    //     alert(`error: ${error}`);
+    // });
+
+    var proxyUrl = proxify(url, {inputFormat: 'xml'});
+
+    console.log(url);
+    console.log(proxyUrl);
+
+    // const yqlUrl = "http://query.yahooapis.com/v1/public/yql";
+    // let q = `SELECT * FROM xml WHERE url="${url}"`;
+
+    
+    $.ajax({
+        type: "get",
+        url: proxyUrl,
+        data: {
+            format: "json"
+        }
+    }).done((yqlResponse) => {
+        console.log("success xxxxxxxxxx");
         // DefiantJS XPath query for user shelf for "read" section.
         let search = JSON.search(yqlResponse, "//*/user_shelf[name='read']");
+        console.log(yqlResponse);
         console.log(search);
     }).fail((request, status, error) => {
         alert(`error: ${error}`);
     });
+
+    // $.ajax({
+    //     type: "get",
+    //     url: yqlUrl,
+    //     data: {
+    //         q: q,
+    //         format: "json"
+    //     }
+    // }).done((yqlResponse) => {
+    //     console.log("success xxxxxxxxxx");
+    //     // DefiantJS XPath query for user shelf for "read" section.
+    //     let search = JSON.search(yqlResponse, "//*/user_shelf[name='read']");
+    //     console.log(yqlResponse);
+    //     console.log(search);
+    // }).fail((request, status, error) => {
+    //     alert(`error: ${error}`);
+    // });
+
+
 });
 
 
